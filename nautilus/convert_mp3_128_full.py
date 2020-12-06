4118c205c1e3ac149947183c616dc2c16d31951e
lad <1081096+lad@users.noreply.github.com>
2020-12-06 16:43:32 +0000

    mp3full/todropbox

--- nautilus/convert_mp3_128_full.py
+++ nautilus/convert_mp3_128_full.py
@@ -4,31 +4,32 @@ import os
 import subprocess
 
 
-for path in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
-  if not path:
-    continue
+def album_artist(path):
+  full_parent, rhs = os.path.split(path)
+  parent_name = os.path.split(full_parent)[1]
 
+  parts=[]
+  while parent_name and parent_name != 'Music':
+    full_parent, part = os.path.split(full_parent)
+    parts.append(part)
+    parent_name = os.path.split(full_parent)[1]
+
+  # If we found the "Music" directory, the last two parts should be the album
+  # and artist name
+  return parts[-2:]
+
+
+def run(path):
   base = os.path.splitext(path)[0]
   filename = os.path.basename(base)
-  print('filename={}'.format(filename))
   output_path = base + '.mp3'
 
-  # get the name of the parent directory and use as the default for the album
-  # name then the name of that parent and use as the default for the artist
-  # name
-  parent = os.path.join(os.path.dirname(path), '..')
-  parent = os.path.realpath(parent)
-  print('parent={}'.format(parent))
-  album = os.path.split(parent)[-1]
-  print('album={}'.format(album))
+  print('output_path={}'.format(filename))
 
-  parent = os.path.join(parent, '..')
-  parent = os.path.realpath(parent)
-  print('parent={}'.format(parent))
-  artist = os.path.split(parent)[-1]
-  print('artist={}'.format(artist))
+  album, artist = album_artist(path)
 
   cmd = 'yad --form --title=MetaData ' \
+        '--width=500 ' \
         '--field=Title ' \
         '--field=Artist ' \
         '--field=Album ' \
@@ -36,30 +37,20 @@ for path in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
         '--field=Of ' \
         '"{}" "{}" "{}"'.format(filename, artist, album)
   print(cmd)
+
   md = subprocess.check_output(cmd, shell=True)
   md = md.decode('utf-8').split("\n")[0]
   title, artist, album, track, of, _ = md.split("|")
 
-  cmd = 'lame --quiet --preset standard --tt "%s" --ta "%s" --tl "%s" --tn "%s/%s" "%s" "%s"' % (title, artist, album, track, of, path, output_path)
+  cmd = 'lame --quiet --preset standard --tt "%s" --ta "%s" --tl "%s" --tn "%s/%s" '\
+        '"%s" "%s"' % (title, artist, album, track, of, path, output_path)
   print("Running...{}".format(cmd))
   subprocess.check_output(cmd, shell=True)
   print('------')
 
 
+for path in os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS'].split('\n'):
+  if not path:
+    continue
 
-  """
-  cmd = 'zenity --forms --title=MetaData ' \
-        '--add-entry=Title --text="%s" ' \
-        '--add-entry=Artist --text="beat cleaver" ' \
-        '--add-entry=Album ' \
-        '--add-entry=Track ' \
-        '--add-entry=Of' % filename
-  md = subprocess.check_output(cmd, shell=True)
-  md = md.split("\n")[0]
-  title, artist, album, track, of = md.split("|")
-
-  cmd = 'lame --quiet --preset standard --tt "%s" --ta "%s" --tl "%s" --tn "%s/%s" "%s" "%s"' % (title, artist, album, track, of, path, output_path)
-  print "Running...", cmd
-  subprocess.check_output(cmd, shell=True)
-  print '------'
-  """
+  run(path)
--- nautilus/to_dropbox_tracks.sh
+++ nautilus/to_dropbox_tracks.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
 FILE="$(echo $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS | tr -d '\n')"
-ls -l  "$FILE"
-
-cp "$(readlink "$FILE")" ~/Dropbox/Tracks/
+cp "$(readlink -f "$FILE")" ~/Dropbox/Tracks/
+[ $? -ne 0 ] && echo "$0: failed" && exit 1
